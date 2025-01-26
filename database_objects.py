1141927a71d353a53189276ea4e7ae91977aa2f3
speedyme11 <speedyme195@yahoo.com>
2025-01-26 13:39:02 -0500

    Add register and improve login route

--- database_objects.py
+++ database_objects.py
@@ -1,17 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import ForeignKey, Integer, String, Text
-from main import app
+from .database import db
 
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
+    """Needed when creating sqlalchemy."""
     pass
 
-db = SQLAlchemy(app=app)
-db.init_app(app=app)
-
 class Users(db.Model):
+    """Create users table."""
+
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    password: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(250), nullable=False)
+    username: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    def __init__(self, email, password, username):
+        super().__init__()
+        self.email = email
+        self.password = password
+        self.username = username
--- main.py
+++ main.py
@@ -1,21 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import LoginForm
-from database_objects import db
+from werkzeug.security import generate_password_hash, check_password_hash
+import bleach
+from .forms import LoginForm, RegisterForm
+from .__init__ import create_app
+from .database import db
+from .database_objects import Users
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "afdsf4ds56f41ds6f1ds5f"
+app = create_app()
 bootstrap = Bootstrap5(app=app)
 
-@app.route("/", methods=["GET"])
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=["GET", "POST"])
 def landing_page():
+    """This will be the landing page when users navigate to the site. A login page."""
+
     form = LoginForm()
+    if form.validate_on_submit():
+        email = db.session.execute(db.select(Users).where(Users.email == form.email.data)).scalar()
+        if not email:
+            flash("Sorry, we could not find an account. Please create an account instead.")
+            return redirect(url_for("register"))
+        else:
+            if email:
+                return "hello"
     return render_template("login.html", form=form)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """This will handle the user registration."""
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = db.session.execute(db.select(Users).where(Users.email == form.email.data)).scalar()
+        if email:
+            flash("You've already signed up with that email. Please login instead.")
+            return redirect(url_for("landing_page"))
+        else:
+            password = generate_password_hash(bleach.clean(form.password.data), method="pbkdf2:sha256", salt_length=16)
+            user = Users(bleach.clean(form.email.data), password, bleach.clean(form.username.data))
+            db.session.add(user)
+            db.session.commit()
+            login_user(user=user)
+            return redirect(url_for("landing_page"))
+        
+    return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
