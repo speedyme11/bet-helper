c63b818a3d23c359beef7544ab139b4eee0f20ef
speedyme11 <speedyme195@yahoo.com>
2025-02-01 20:25:57 -0500

    Tinker with css, update and add requests and bs4 code

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 """Import the libraries."""
 import os
+import requests
+import json
+from bs4 import BeautifulSoup
 from flask import Flask
 from .database import db
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
+with open(file="headers.json", mode="r", encoding="utf-8") as file:
+    HEADERS = json.load(file)
 
 def create_app():
     """Initialize the flask application."""
@@ -16,3 +21,16 @@ def create_app():
     db.init_app(app=app)
 
     return app
+
+class WebScraping:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.headers = HEADERS.get("headers")
+
+    def get_beautiful(self, url: str):
+        self.response = requests.get(url=url, headers=self.headers)
+        self.response.raise_for_status()
+
+        soup = BeautifulSoup(self.response.text, "html.parser")
+        return soup
